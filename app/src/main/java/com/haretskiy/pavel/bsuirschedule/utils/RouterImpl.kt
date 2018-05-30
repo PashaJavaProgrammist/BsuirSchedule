e588731402171a52c64badcb9192750e78aed37f
Pavel <pasha280294@gmail.com>
2018-05-30 11:22:44 +0300

    dagger refs

--- app/src/main/java/com/haretskiy/pavel/bsuirschedule/utils/RouterImpl.kt
+++ app/src/main/java/com/haretskiy/pavel/bsuirschedule/utils/RouterImpl.kt
@@ -5,9 +5,8 @@ import com.haretskiy.pavel.bsuirschedule.BUNDLE_KEY_NUMBER_GROUP
 import com.haretskiy.pavel.bsuirschedule.ui.activities.BaseActivity
 import com.haretskiy.pavel.bsuirschedule.ui.activities.GroupsActivity
 import com.haretskiy.pavel.bsuirschedule.ui.activities.ScheduleActivity
-import javax.inject.Inject
 
-class RouterImpl @Inject constructor(private val activity: BaseActivity) : Router {
+class RouterImpl(private val activity: BaseActivity) : Router {
 
     override fun startScheduleActivity(numberOfGroup: String) {
         val intent = Intent(activity, ScheduleActivity::class.java)
